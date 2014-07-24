b15185734a2783d12f53f7dd1cd5a5b15f14edec
renwill <renwill.yang@gmail.com>
2014-07-24 22:22:10 +0800

    2nd submission with added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
+# Description: 
+##  makeCacheMatrix creates a list of functions to store & retrieve a matrix & an optional LHS column vector of a linear system and the solution
+## If the optional RHS column vector is missing, the cached solution represents the inverse of the matrix
+## The output list is for use with the cacheSolve function to cache the solution to avoid re-computation when we need the solution again
+# Usage
+## makeCacheMatrix(a, b = NULL)
+# Arguments
+## 'a' is the matrix to cache 
+## 'b' is an optional right-hand-side column vector of the linear system
+# Return
+## the result return is a list of four functions 
+## - set(a, b): set the matrix 'a' and the optional RHS column vector 'b'
+## - get() returns the matrix cached
+## - getRHS() returns the RHS vector cached
+## - setLinearSolution(i): sets the computed solution i (which is the matrix inverse if 'b' is null)
+## - getLinearSolution() returns the cached solution (which is the matrix inverse if 'b' is null)
+makeCacheMatrix <- function(a = matrix(), b = NULL) {
     i <- NULL
-    get <- functon() x
-    getInverse <- function() i
-    set <- function(y) {
-        x <<- y
+    get <- function() a
+    getRHS <- function() b
+    getLinearSolution <- function() i
+    set <- function(y, z = NULL) {
+        a <<- y
+        b <<- z
         i <<- NULL
     }
-    setInverse <- function(inverseX) i <- inverseX
-    list (set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+    setLinearSolution <- function(solveLinear) i <<- solveLinear
+    list (set = set, get = get, getRHS = getRHS, setLinearSolution = setLinearSolution, getLinearSolution = getLinearSolution)
 }
 
-
-## Write a short comment describing this function
-
+# Description
+## cacheSolve provides the solution of a %*% y = b for y if b is not null 
+##            and returns the inverse matrix of a if b is null,
+## where 'x' is special list of functions returned from makeCacheMatrix(a,b)
+## If the solution has been computed before, it will not be recomputed and the
+## cached result will be returned
+# Usage
+## cacheSolve(x)
+# Arguments
+## 'x' is the list of functions returned from makeCacheMatrix(a, b)
+# Return
+## * the solution of a %*% y = b for y if b is not null
+## * the inverse matrix of a           if b is null
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
-    i <- x$getInverse()
+    i <- x$getLinearSolution()
     if (!is.null(i)) {
-        message("getting cached inverse of the matrix")
+        message("getting the cached result")
         return(i)
     }
-    m <- x$get()
-    i <- solve(m)
-    x$setInverse(i)
+    a <- x$get()
+    b <- x$getRHS()
+    if (is.null(b)) {
+        i <- solve(a, ...)
+    } else {
+        i <- solve(a, b, ...)
+    }
+    x$setLinearSolution(i)
     i
 }
